1976ae015bd47a3368ff391d3d681df6f5ac0992
Leandro Antonio <eng.leandro.a@gmail.com>
2022-03-09 20:07:56 -0300

    Criação do arquivo aula3, insere condicional if

---
+++ aula3.py
@@ -0,0 +1,11 @@
+"""Aula 3 - """
+a = int(input('Primeiro valor: '))
+b = int(input('Segundo valor: '))
+c = int(input('Terceiro valor: '))
+if a > b and a > c:
+    print(f'o maior número é {a}')
+elif b > a and b > c:
+    print(f'O maior número é {b}')
+else:
+    print(f'O maior número é {c}')
+print("Fim do programa")
