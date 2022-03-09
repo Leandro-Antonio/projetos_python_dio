1cfd36990dd0a9c7cb4dde2d95a8dd9d6cde5251
Leandro Antonio <eng.leandro.a@gmail.com>
2022-03-09 18:53:12 -0300

    adicionado entrada do usuário com input

--- aula2.py
+++ aula2.py
@@ -2,12 +2,26 @@
 a = 10
 b = 5
 soma = a + b
-subtracao = a - b
-multiplicacao = a * b
-divisao = a / 2
+subt = a - b
+multi = a * b
+divis = a / b
 resto = a % b
-print(soma)
-print(subtracao)
-print(multiplicacao)
-print(divisao)
-print(resto)
+print(f'Soma: {soma}. Subtracao: {subt}')
+print('soma: ' + str(soma))
+print('subtração ' + str(subt))
+print('multiplicação ' + str(multi))
+print('divisão ' + str(divis))
+print('resto ' + str(resto))
+"""converte a string x em inteiro para possibilitar a operação"""
+x = '1'
+soma2 = int(x) + 1
+print(soma2)
+
+a = int(input('Entre com o primeiro valor: '))
+b = int(input('Entre com o segundo valor: '))
+print(f'Soma: {soma}. Subtracao: {subt}')
+print('soma: ' + str(soma))
+print('subtração ' + str(subt))
+print('multiplicação ' + str(multi))
+print('divisão ' + str(divis))
+print('resto ' + str(resto))
