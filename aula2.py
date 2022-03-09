d7089abca13c6f47979c8ab6b094a765658e9dea
Leandro Antonio <eng.leandro.a@gmail.com>
2022-03-09 16:52:41 -0300

    Criação do arquivo aula2 e estudando algumas operações básicas

---
+++ aula2.py
@@ -0,0 +1,13 @@
+"""Aula 2 - Operações básicas"""
+a = 10
+b = 5
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / 2
+resto = a % b
+print(soma)
+print(subtracao)
+print(multiplicacao)
+print(divisao)
+print(resto)
