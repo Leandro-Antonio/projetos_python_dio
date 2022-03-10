7406eab1edec3d1ad2bc850fc7c9e547fc7802eb
Leandro Antonio <eng.leandro.a@gmail.com>
2022-03-10 16:29:20 -0300

    Acrescenta condicional for

---
+++ aula4.py
@@ -0,0 +1,11 @@
+"""Aula 4"""
+
+"""Percorre de 0 a 100 e exibe cada passada """
+for x in range(101):
+    print(x)
+print('fim ')
+
+"""Percorre de 90 até 100 e exibe cada passada"""
+for y in range(90, 100):
+    print(y)
+print("fim")
