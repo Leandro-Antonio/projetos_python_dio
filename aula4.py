446a7bf6ad0bc7d4f5d645be0958b4f375964eaa
Leandro Antonio <eng.leandro.a@gmail.com>
2022-03-10 18:39:11 -0300

    Calcula se um número é par ou ímpar

--- aula4.py
+++ aula4.py
@@ -5,7 +5,19 @@ for x in range(101):
     print(x)
 print('fim ')
 
-"""Percorre de 90 até 100 e exibe cada passada"""
+"""Percorre de 90 até 109 e exibe cada passada"""
 for y in range(90, 100):
     print(y)
 print("fim")
+
+div = 0
+numero = int(input("Digite um número"))
+for z in range(1, numero + 1):
+    resto = numero % z
+    if resto == 0:
+        print(numero, resto)
+        div = div + 1
+if div == 2:
+    print(f'número {numero} é primo')
+else:
+    print(f'número {numero} não é primo')
