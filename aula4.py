f7e677e9d64bbb243fa2fca55830e29ee23bbf70
Leandro Antonio <eng.leandro.a@gmail.com>
2022-03-11 07:53:47 -0300

    Testa se um número é primo

--- aula4.py
+++ aula4.py
@@ -9,3 +9,28 @@ print('fim ')
 for y in range(90, 100):
     print(y)
 print("fim")
+
+# """Testa se um número é primo """
+# div = 0
+# numero = int(input("Digite um número"))
+# for z in range(1, numero + 1):
+#     resto = numero % z
+#     if resto == 0:
+#         print(numero, resto)
+#         div += 1
+# if div == 2:
+#     print(f'número {numero} é primo')
+# else:
+#     print(f'número {numero} não é primo')
+
+"""Testa se um número é primo """
+
+for numero in range(100):
+    div = 0
+    for z in range(1, numero + 1):
+        resto = numero % z
+       # print(z, resto)
+        if resto == 0:
+            div += 1
+    if div == 2:
+        print(numero)
