adeb8b2fcff9de87fc6c1cc3ab7337379ddf9590
Leandro Antonio <57641579+Leandro-Antonio@users.noreply.github.com>
2022-03-10 22:04:44 -0300

    Update aula4.py
    Adicionado algumas descrições no código

--- aula4.py
+++ aula4.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 """Aula 4 condicional for, testa se um número é primo"""
-=======
-"""Aula 4"""
->>>>>>> refs/remotes/origin/main
 
 """Percorre de 0 a 100 e exibe cada passada """
 for x in range(101):
@@ -19,6 +15,7 @@ for y in range(90, 100):
     print(y)
 print("fim")
 
+"""Testa se um número é primo """
 div = 0
 numero = int(input("Digite um número"))
 for z in range(1, numero + 1):
